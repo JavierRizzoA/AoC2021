17a7ab0163c76bad07634377410504b6e841202d
Javier Rizzo <javierrizzoa@gmail.com>
2021-12-10 11:29:17 -0800

    Day 5

---
+++ Day05/part1.py
@@ -0,0 +1,33 @@
+plane = {}
+
+def add_to_plane(x, y):
+    if not x in plane:
+        plane[x] = {}
+    if not y in plane[x]:
+        plane[x][y] = 1
+    else:
+        plane[x][y] += 1
+
+
+def count_overlaps(plane):
+    overlaps = 0
+    for x, val in plane.items():
+        for y, overlap in val.items():
+            if overlap > 1:
+                overlaps += 1
+    return overlaps
+
+
+with open('input') as f:
+    for line in f:
+        start, end = tuple(line.split(' -> '))
+        x1, y1 = map(int, start.split(','))
+        x2, y2 = map(int, end.split(','))
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                add_to_plane(x1, y)
+        elif y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                add_to_plane(x, y1)
+
+print(count_overlaps(plane))
